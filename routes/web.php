8dfada0d4c67fdd981f8c25d1e6f0e4aa68024bb
Alexander Melihov <amelihovv@ya.ru>
2017-11-11 20:47:41 +0300

    remove unnecessary files

--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,4 @@ Route::get('/logs', [
     'as' => 'logs',
 ]);
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
